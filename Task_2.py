46a69a39b8b7f61546b148642426477d1d05bdaa
FroL1995 <137566026+FroL1995@users.noreply.github.com>
2024-11-03 18:11:45 +0000

    Модуль_2 Домашнее задание_4

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+numbers = [int(i) for i in input('Введите три числа: ') if i.isdigit()]
+sign = input("Введите знак")
+if sign == '*':
+    for i in numbers:
+        multiple *=i
+    print(multiple)
+elif sign == '+':
+    summa
+    
