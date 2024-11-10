1a2666d2abcb540ffe0925128269e368c0a23914
FroL1995 <137566026+FroL1995@users.noreply.github.com>
2024-11-10 17:08:36 +0000

    dz7 task_3

---
+++ dz7/Task_2.py
@@ -0,0 +1,27 @@
+num1=int(input('Введите первое число: '))
+num2=int(input('Введите второе число: '))
+
+# Все числа диапазона через цикл
+
+# for i in range(num1, num2 + 1):
+#     print(i)
+
+# Все числа диапазона # через список
+
+# user_list = [i for i in range(num1, nym2+1)]
+# print(*user_list)
+
+# От максимального к минимальному # через список
+
+# user_list = [i for i in range(num1, nym2+1)]
+# print(*user_list[::-1])
+
+# Кратные семи # через список
+
+# user_list = [i for i in range(num1, nym2+1)]
+# print(*[i for i in user_list if i % 7 == 0])
+
+# Количество кратных пяти # через список
+
+# user_list = [i for i in range(num1, nym2+1)]
+# print(len([i for i in user_list if i % 5 == 0]))
