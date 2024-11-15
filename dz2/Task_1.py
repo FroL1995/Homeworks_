2e4d3a8b43d6feaf7825c27291cd065b0b552411
FrolMac <frolich.slokva@gmail.com>
2024-11-16 00:13:22 +0300

    dz2_task_1

--- dz2/Task_1.py
+++ dz2/Task_1.py
@@ -13,8 +13,10 @@
 
 # Вариант_2
 
-my_list = [int(i) for i in input("Введите число").split()]
-
-print(sum(my_list))
-
-
+my_list = [int(i) for i in input("Введите число").split(",")]
+summa = 0
+proisvedenie = 1
+for i in my_list:
+    summa += i
+    proisvedenie *= i
+print(summa, proisvedenie)
